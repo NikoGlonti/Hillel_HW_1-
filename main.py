11d014222a9c2f87b918d4b57c9a1bf9659f80dd
”NikoGlonti” <niko.guria@gmail.com>
2021-12-02 01:22:57 +0200

    update function parse

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
 def parse(query: str) -> dict:
-    return {}
+    response = dict()
+    my_query = query.split('?', 1)  # сплитим входящую строку по ? знак
+    try:
+        temp_str = ''.join(my_query[1])  # создаем новую строку и отбрасываемы все что было до ?
+    except IndexError:
+        return {}
 
+    temp_query = temp_str.split('&')  # и снова сплитим нашу новую строку по &
+    for i in temp_query:  # в цикле перебираем наш результат сплитим и записывем в словарь ключ значения
+        temp = i.split('=')
+        try:
+            response[temp[0]] = temp[1]
+        except IndexError:
+            continue
+    res = {k: v for k, v in response.items() if k}  # исключаем возможность записи пустого ключа
+    return res
+
+
+print(parse('https://example.com/path/to/page?name=ferret&color=purple'))
 
 if __name__ == '__main__':
     assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
@@ -8,6 +25,8 @@ if __name__ == '__main__':
     assert parse('http://example.com/') == {}
     assert parse('http://example.com/?') == {}
     assert parse('http://example.com/?name=Dima') == {'name': 'Dima'}
+    assert parse('http://example.com/?&&&') == {}
+    assert parse('https://example.com/path/to/page?name=ferret&color=purple') == {'name': 'ferret', 'color': 'purple'}
 
 
 def parse_cookie(query: str) -> dict:
